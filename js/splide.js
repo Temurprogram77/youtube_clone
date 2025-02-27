2ad6abc30d19662ccebafc7ef31e060d147df806
Temur Coder <temurnarzullayev810@gmail.com>
2025-02-27 17:27:23 +0300

    create splide.js and added code

---
+++ js/splide.js
@@ -0,0 +1,10 @@
+var splide1 = new Splide(".splide", {
+    type: "loop",
+    snap: true,
+    arrows: false,
+    autoplay: true,
+    focus: "start",
+    perpage: 1,
+  });
+  splide1.mount();
+  
