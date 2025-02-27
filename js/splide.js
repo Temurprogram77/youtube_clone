7c7bd59d72b7d1d742bebf8c3ea4ba2a66f99f03
Temur Coder <temurnarzullayev810@gmail.com>
2025-02-27 17:28:17 +0300

    prettier building

--- js/splide.js
+++ js/splide.js
@@ -1,10 +1,9 @@
 var splide1 = new Splide(".splide", {
-    type: "loop",
-    snap: true,
-    arrows: false,
-    autoplay: true,
-    focus: "start",
-    perpage: 1,
-  });
-  splide1.mount();
-  
+  type: "loop",
+  snap: true,
+  arrows: false,
+  autoplay: true,
+  focus: "start",
+  perpage: 1,
+});
+splide1.mount();
