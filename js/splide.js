7381f6aa251bf7bda7833eca54d42dafa76fc9e5
Temur Coder <temurnarzullayev810@gmail.com>
2025-02-27 17:41:59 +0300

    slider is being corrected

--- js/splide.js
+++ js/splide.js
@@ -1,9 +1,9 @@
-var splide1 = new Splide("#splide1", {
+var splide1 = new Splide(".splide", {
   type: "loop",
   snap: true,
   arrows: false,
 //   autoplay: true,
   focus: "start",
-  perpage: 1,
+  perpage: 10,
 });
 splide1.mount();
